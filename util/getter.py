e6914bb39a9919c98a9f8f7397a299bcc2f957f3
Emil Jaffal <emiljaffal@yahoo.com>
2024-07-14 18:50:44 -0400

    Fixed Getter, Added NaCl example
    Previous Getter.py 'or' didn't work with glob

--- util/getter.py
+++ util/getter.py
@@ -4,30 +4,24 @@ import click
 
 
 def get_cohp_files(directory):
-    # Use glob to find all files starting with "COHP-" in the specified directory
-    cohp_files = glob.glob(
-        os.path.join(directory, "COHP-*" or "cohp-*" or "Cohp-*")
-    )
+    # Use glob to find all files starting with "COHP-", "cohp-", or "Cohp-" in the specified directory
+    cohp_files = glob.glob(os.path.join(directory, "[Cc][Oo][Hh][Pp]-*"))
     if not cohp_files:
         click.echo("COHP/COOP files not found.")
     return cohp_files
 
 
 def get_coop_files(directory):
-    # Use glob to find all files starting with "COOP-" in the specified directory
-    coop_files = glob.glob(
-        os.path.join(directory, "COOP-*" or "coop-*" or "Coop-*")
-    )
+    # Use glob to find all files starting with "COOP-", "coop-", or "Coop-" in the specified directory
+    coop_files = glob.glob(os.path.join(directory, "[Cc][Oo][Oo][Pp]-*"))
     if not coop_files:
         click.echo("COOP files not found.")
     return coop_files
 
 
 def get_dos_files(directory):
-    # Use glob to find all files starting with "Dos-" in the specified directory
-    dos_files = glob.glob(
-        os.path.join(directory, "Dos-*" or "DOS-*" or "dos-*")
-    )
+    # Use glob to find all files starting with "DOS-", "dos-", or "Dos-" in the specified directory
+    dos_files = glob.glob(os.path.join(directory, "[Dd][Oo][Ss]-*"))
     if not dos_files:
         click.echo("DOS files not found.")
     return dos_files
